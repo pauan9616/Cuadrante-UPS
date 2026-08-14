a9af4cd907875b920276e400219a2e08e9dadcf7
pauan9616 <pabloandres9616@gmail.com>
2026-08-14 11:27:47 +0200

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD2hz1w4Vz5mozP5HtNvu51vXjueAgZKoY",
-  authDomain: "cuadrante-ups.firebaseapp.com",
-  projectId: "cuadrante-ups",
-  storageBucket: "cuadrante-ups.firebasestorage.app",
-  messagingSenderId: "915293743085",
-  appId: "1:915293743085:web:53e271895d934947d529e4",
-};
